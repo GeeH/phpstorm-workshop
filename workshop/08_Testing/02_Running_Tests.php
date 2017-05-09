129a643821542cc44444ff925a999f54f6734679
Gary <gary@hock.in>
2017-05-09 10:59:06 +0100

    WIP

--- workshop/08_Testing/02_Running_Tests.php
+++ workshop/08_Testing/02_Running_Tests.php
@@ -2,26 +2,24 @@
 /**
  * Running tests
  *
- * Run Unit Tests using PHPUnit. Note that the PHPUnit framework can be acquired through PEAR or Composer.
+ * Run Unit Tests using PHPUnit.
+ * NOTE: PHPUnit framework can be acquired through Composer.
  */
 
 namespace Testing\JetBrains;
 
-// 1. Create a new Run Configuration based on PHPUnit. Name it "Run tests" and specify the directory to look for tests:
+// 1. Create a new Run Configuration based on PHPUnit. Name it "Run Tests" and specify the directory to look for tests:
 //      \08_Testing\
-//    Note that a specific class or method can also be run.
+//    Note that a specific Class or Method can also be run.
 //    A PHPUnit XML configuration file can also be specified for this specific run configuration.
 // 2. PhpStorm has already preconfigured PHPUnit configuration for Remote Interpreter in Settings | PHP | PHPUnit so
 //    you don't need to set up anything. 
-// 3. Almost there! The Queue class can not be loaded. To solve this, do one of the following:
-//
-//      Add the line require_once 'Code/Queue.php';
-//
-//      -or-
-//
-//      From the Project Settings | PHP | PHPUnit, set \08_Testing\Code\Bootstrap.php as the default bootstrap file.
+// 3. Almost there! The Queue class can not be loaded. To solve this, do the following:
+//      From the Settings Pane, select Languages & Frameworks| PHP | PHPUnit
+//      Set \08_Testing\Code\Bootstrap.php as the default bootstrap file.
 //      A bootstrap file can be used to run custom PHP code in the unit test runner. Bootstrap.php includes all required files
 //      so that the tests can be run.
-// 4. Execute the Run Configuration.
-// 5. The Test tool window below shows test results. Using the context menu,
-//    additional statistics such as the number of assertions can be viewed.
+//      NOTE: If you're using Composer to autoload in your projects you can set `vendor/autoload.php` as the bootstrap file
+// 4. Execute the Run Configuration (press the green Play icon or Shift+F10 / Ctrl+R)
+// 5. The Test tool window below shows test results. Using the context menu, additional statistics such as the number of
+// assertions can be viewed.
